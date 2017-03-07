44c44bc61d22c385a8c8f93d5610286bfa4c963e
rmcdesign <info@rmc-design.co.uk>
2017-03-07 22:27:45 +0000

    Updated filtering

--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -61,7 +61,7 @@ describe('TodoAPI', () => {
 
         var todos = [{
             id: 1,
-            text: 'Test 1',
+            text: 'Some text here',
             completed: true
         }, {
             id: 1,
@@ -69,7 +69,7 @@ describe('TodoAPI', () => {
             completed: false
         }, {
             id: 1,
-            text: 'Test 3',
+            text: 'Some other text',
             completed: true
         }];
 
@@ -81,6 +81,22 @@ describe('TodoAPI', () => {
         it('should return non-completed todos when showCompleted is false', () => {
             var filteredTodos = TodoAPI.filterTodos(todos, false, '');
             expect(filteredTodos.length).toBe(1);
+        });
+
+        it('should sort by completed status', () => {
+            var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+            expect(filteredTodos[0].completed).toBe(false);
+        });
+
+        it('should filter todos by search text', () => {
+            var filteredTodos = TodoAPI.filterTodos(todos, true, 'some');
+            expect(filteredTodos.length).toBe(2);
+        });
+
+
+        it('should return all todos if search text is empty', () => {
+            var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+            expect(filteredTodos.length).toBe(3);
         });
 
     });
